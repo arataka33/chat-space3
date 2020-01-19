6381a830f1ba1411f656aeec8e34b13b30838775
arataka33 <59165182+arataka33@users.noreply.github.com>
2020-01-19 16:47:48 +0900

    自動更新機能の追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    var img = message.image ? `<img src= ${message.image}>` : "";
+    var html = `<div class="message" data-message-id="${message.id}">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">${message.user_name}</div>
+                    <div class="message__upper-info__date">${message.created_at}</div>
+                  </div>
+                  <div class="message__text">
+                    <p class="lower-message__content">${message.content}</p>
+                    ${img}
+                  </div>
+                </div>`
+    return html;
+  };
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url ,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+     .done(function(data){
+       var html = buildHTML(data);
+       $('.messages').append(html); 
+       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');  
+       $('form')[0].reset(); 
+     })
+      .fail(function(){
+       alert('メッセージ送信に失敗しました');
+     });
+     return false;
+    
+  });
+  last_message_id = $('.message:last').data("message-id");
+  // console.log(last_message_id);
     function buildHTML(message){
       if ( message.image ) {
         var html =
@@ -39,7 +80,7 @@ $(function(){
         return html;
       };
     }
-$('#new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -61,5 +102,40 @@ $('#new_message').on('submit', function(e){
        alert('メッセージ送信に失敗しました');
      });
      return false;
-    });
+    
+  });
+  var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+        //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $("#new_message")[0].reset();
+      $(".form__submit").prop("disabled", false);
+    }
+  })
+  .fail(function() {
+    console.log('error');
+  });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
